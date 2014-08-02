6359ec6a0bd23c870e3c3d6a42cdb1048f984ac9
A. Alfonso F. R. <alffore@gmail.com>
2014-08-02 09:13:53 -0500

    Se agrega un proyecto básico con  fines de prueba
    Este código ilustra una aplicacion basica incorrecta / correcta de
    Threadings in C# (simple test)

---
+++ PruebaThread1/PruebaThread1/CTrabajo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaThread1
+{
+    class CTrabajo
+    {
+
+        protected int v;
+        protected int tarea;
+       
+
+        public CTrabajo(int tarea,int v)
+        {
+            this.v = v;
+            this.tarea = tarea;
+           
+        }
+
+        // metodo que checa como trabajo si un número es primo o compuesto por divisibilidad
+        public void hasTrabajo()
+        {
+            String sres;
+            int divi = 0;
+
+            for (int i = v; i > 0; i--)
+            {
+                if (v % i == 0)
+                {
+                    divi++;
+                    if (divi > 2) break;
+                }
+            }
+
+            if (divi == 2)
+            {
+                sres = "primo";
+            }
+            else
+            {
+                sres = "compuesto";
+            }
+            
+            System.Console.WriteLine(tarea+":: "+v + ": " + sres);
+        }
+
+
+    }
+}
---
+++ PruebaThread1/PruebaThread1/MainT.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaThread1
+{
+    class MainT
+    {
+        Random r = new Random();
+
+        static int tam = 100;
+
+        int[] dato_n = new int[tam];
+
+        public MainT()
+        {
+            for (int i = 0; i < tam; i++)
+            {
+                dato_n[i] = r.Next(1, 100000000);
+            }
+
+            creaT();
+            creaMT();
+        }
+
+
+        // metodo que solo crea un hilo para el trabajo
+        private void creaT()
+        {
+            for (int i = 0; i < tam; i++)
+            {
+                CTrabajo ct = new CTrabajo(i, dato_n[i]);
+                ct.hasTrabajo();
+            }
+        }
+
+        // metodo que crea todos los posibles multihilos para el trabajo
+        private void creaMT()
+        {
+
+            for (int i = 0; i < tam; i++)
+            {
+                CTrabajo ct = new CTrabajo(i, dato_n[i]);
+                System.Threading.Thread Thread = new System.Threading.Thread(ct.hasTrabajo);
+                Thread.Start();
+            }
+        }
+
+        //metodo principal
+        static void Main(string[] args)
+        {
+            new MainT();
+        }
+    }
+}
